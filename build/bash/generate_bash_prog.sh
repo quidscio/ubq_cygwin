0fe5acc040b4ece460988aa143191938cf19d603
quidscio <rmhines@polaris360.com>
2025-12-02 20:57:08 -0500

    Back out first change attempt

--- build/bash/generate_bash_prog.sh
+++ build/bash/generate_bash_prog.sh
@@ -1,21 +1,4 @@
 _generate_compile_bash_prog() {
-	
-	"$scriptAbsoluteLocation" _compile_bash rOverrides.sh 
-	
-	return
-	# Legacy code below, kept for reference 
-		
-	"$scriptAbsoluteLocation" _true	 		# If no compiles are needed, this must be called before return 
-	
-	rm "$scriptAbsoluteFolder"/ubiquitous_bash.sh
-	
-	#"$scriptAbsoluteLocation" _compile_bash cautossh cautossh
-	#"$scriptAbsoluteLocation" _compile_bash lean lean.sh
-	
-	"$scriptAbsoluteLocation" _compile_bash core ubiquitous_bash.sh
-	
-	#"$scriptAbsoluteLocation" _compile_bash "" ""
-	#"$scriptAbsoluteLocation" _compile_bash ubiquitous_bash ubiquitous_bash.sh
-	
-	#"$scriptAbsoluteLocation" _package
+	"$scriptAbsoluteLocation" _true
 }
+
